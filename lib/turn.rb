ed16a2457dce8d7c660d9e4109558936299d9e59
Mohamad Khayat <moek91@outlook.com>
2018-03-28 23:09:30 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+#will move the board piece
 def input_to_index(input)
   position = input.to_i - 1
 end
@@ -14,6 +15,25 @@ def move(board, position, letter="X")
   board[position] = letter
 end
 
-def valid_move?(board, index)
+def position_taken?(board,index)
+ board[index] == "X" || board[index] ==  "O"
+end
+
+def valid_move? (board, index)
   return index <= 8  && index >= 0 &&  !position_taken?(board, index)
+end
+
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets
+  index = input_to_index(input)
+  if valid_move?(board,index)
+    move(board, index)
+    display_board(board)
+  else
+     turn(board)
+  end
+
 end
